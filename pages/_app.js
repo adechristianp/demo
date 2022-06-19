b1f6ff3c5186e94aa4d83f89ecb9d4c9051f51f0
Bangkit Finaldi <bangkitfinaldi@gmail.com>
2022-06-19 18:49:46 +0700

    remove emotion cache from app wrapper due to ui blip on reload page

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,12 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
 import { Provider } from 'react-redux';
 import { useRouter } from 'next/router';
 
 import store from '../reducer/store';
 import theme from '../styles/theme';
-import createEmotionCache from '../styles/createEmotionCache';
 import LocalDataProvider from './LocalDataProvider';
-
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
 
 const client = new ApolloClient({
   uri: 'https://graphql.anilist.co/',
@@ -23,9 +18,7 @@ const client = new ApolloClient({
 
 const ROUTES_TO_RETAIN = ['/'];
 
-const MyApp = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
+const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
   const retainedComponents = useRef({})
 
@@ -65,32 +58,30 @@ const MyApp = (props) => {
   }, [Component, pageProps])
 
   return (
-    <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={theme}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <ApolloProvider client={client} >
-          <Provider store={store}>
-            <LocalDataProvider>
-              <div style={{ display: isRetainableRoute ? 'block' : 'none' }}>
-                {Object.entries(retainedComponents.current).map(([path, c]) => (
-                  <div
-                    key={path}
-                    style={{ display: router.asPath === path ? 'block' : 'none' }}
-                  >
-                    {c.component}
-                  </div>
-                ))}
-              </div>
-              {!isRetainableRoute && <Component {...pageProps} router={router} />}
-            </LocalDataProvider>
-          </Provider>
-        </ApolloProvider >
-      </ThemeProvider>
-    </CacheProvider>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <ApolloProvider client={client} >
+        <Provider store={store}>
+          <LocalDataProvider>
+            <div style={{ display: isRetainableRoute ? 'block' : 'none' }}>
+              {Object.entries(retainedComponents.current).map(([path, c]) => (
+                <div
+                  key={path}
+                  style={{ display: router.asPath === path ? 'block' : 'none' }}
+                >
+                  {c.component}
+                </div>
+              ))}
+            </div>
+            {!isRetainableRoute && <Component {...pageProps} router={router} />}
+          </LocalDataProvider>
+        </Provider>
+      </ApolloProvider >
+    </ThemeProvider>
   )
 }
 
